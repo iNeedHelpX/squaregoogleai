dd632866ebc9176488cb9d7ac2c9bf9ef66c9dea
iNeedHelpX <jpak0444@gmail.com>
2023-09-16 23:11:37 -0400

    pod

--- lib/models/login.dart
+++ lib/models/login.dart
@@ -1,1 +1,58 @@
 //firebase login
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  static const ID = "id";
+  static const NAME = "name";
+  static const EMAIL = "email";
+  static const CART = "cart";
+  static const PHOTOURL = "photoURL";
+  static const REVIEWS = "reviews";
+
+  String? id;
+  String? name;
+  String? email;
+  String? photoURL;
+  // List<CartItemModel>? cart;
+  // List<ReviewModel>? reviews;
+
+  UserModel({
+    this.id,
+    this.photoURL,
+    this.email,
+    this.name,
+    // this.cart,
+    // this.reviews,
+  });
+
+  UserModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    name = snapshot.data()?['NAME'] ?? '';
+    email = snapshot.data()?['EMAIL'] ?? '';
+    photoURL = snapshot.data()?['PHOTOURL'] ?? '';
+    id = snapshot.data()?['ID'] ?? '';
+    // cart = _convertCartItems(snapshot.data()?['CART'] ?? []);
+    // reviews = _convertCartItems(snapshot.data()?['REVIEWS'] ?? []);
+  }
+
+  // List<CartItemModel> _convertCartItems(List cartFomDb) {
+  //   List<CartItemModel> result = [];
+  //   if (cartFomDb.isNotEmpty) {
+  //     for (var element in cartFomDb) {
+  //       result.add(CartItemModel.fromMap(element));
+  //     }
+  //   }
+  //   return result;
+  // }
+
+  // List cartItemsToJson() => cart!.map((item) => item.toJson()).toList();
+
+  Map<String, dynamic> toJson() => {
+        "uid": id,
+        "email": email,
+        "name": name,
+        "photoUrl": photoURL,
+        // "cart": cart,
+        // "reviews": reviews,
+      };
+}
--- lib/views/loginpage.dart
+++ lib/views/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gradients_reborn/flutter_gradients_reborn.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,11 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: FlutterGradients.lilyMeadow(tileMode: TileMode.clamp),
+      ),
+      child: Stack(),
+    );
   }
 }
