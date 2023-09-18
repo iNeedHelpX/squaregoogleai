56bf4791172401ccb87f22747f48fac7f2d4592b
iNeedHelpX <jpak0444@gmail.com>
2023-09-17 23:53:31 -0400

    updated class

--- lib/services/url_helper.dart
+++ lib/services/url_helper.dart
@@ -1,4 +1,7 @@
+import 'dart:js_util';
+
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 //helper class to launch a url
 class UrlHelper {
@@ -6,6 +9,19 @@ class UrlHelper {
   static Future<void> launchUrl(String url) async {
     if (await canLaunch(url)) {
       await launch(url, enableJavaScript: true);
+    }
+  }
+}
+
+//updated class
+class UrlHelp {
+  static Future<void> launchUrl(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrl(
+        url,
+        java,
+        enableJavaScript: true,
+      );
     }
   }
 }
