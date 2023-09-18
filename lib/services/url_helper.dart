4b37f345c278b36db7cfaff5a042a003f7f5ed68
iNeedHelpX <jpak0444@gmail.com>
2023-09-17 23:49:10 -0400

    added comments and dependency

--- lib/services/url_helper.dart
+++ lib/services/url_helper.dart
@@ -1,6 +1,8 @@
 import 'package:url_launcher/url_launcher.dart';
 
+//helper class to launch a url
 class UrlHelper {
+  //helps to launch a url
   static Future<void> launchUrl(String url) async {
     if (await canLaunch(url)) {
       await launch(url, enableJavaScript: true);
